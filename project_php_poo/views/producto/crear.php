ec6f2aee06c2a45968a01e54b5fd43b1b1b4b3de
Jacanroman <jacanroman@gmail.com>
2020-11-14 11:44:09 +0000

    update products

--- project_php_poo/views/producto/crear.php
+++ project_php_poo/views/producto/crear.php
@@ -1,29 +1,48 @@
-<h1>Crear nuevos productos</h1>
+<?php if(isset($edit) && isset($pro) && is_object($pro)): ?>
+    <h1>Editar producto   <?=$pro->nombre?></h1>
 
-<form action="<?=base_url?>producto/save" method="POST" class="form_container" enctype="multipart/form-data">
+    <?php
+    $url_action = base_url."producto/edit&id=".$pro->id;
+    ?>
+<?php else: ?>
+    <h1>Crear Nuevo productos</h1>
+
+    <?php
+    $url_action = base_url."producto/save";
+    ?>
+<?php endif; ?>
+
+
+
+
+<form action="<?=$url_action?>" method="POST" class="form_container" enctype="multipart/form-data">
     <label for="nombre">Nombre</label>
-    <input type="text" name="nombre" />
+    <input type="text" name="nombre" value="<?=isset($pro) && is_object($pro) ? $pro->nombre : ''?>" />
 
     <label for="descripcion">Descripcion</label>
-    <textarea name="descripcion"></textarea>
+    <textarea name="descripcion"><?=isset($pro) && is_object($pro) ? $pro->descripcion : ''?></textarea>
 
     <label for="precio">Precio</label>
-    <input type="text" name="precio" />
+    <input type="text" name="precio" value="<?=isset($pro) && is_object($pro) ? $pro->precio : ''?>" />
 
     <label for="stock">Stock</label>
-    <input type="number" name="stock" />
+    <input type="number" name="stock" value="<?=isset($pro) && is_object($pro) ? $pro->stock : ''?>" />
 
     <label for="categoria">Categoria</label>
     <?php $categorias = Utils::showCategorias(); ?>
+
     <select name="categoria">
         <?php while($cat = $categorias->fetch_object()): ?>
-            <option value="<?=$cat->id?>">
+            <option value="<?=$cat->id?>" <?=isset($pro) && is_object($pro) && $cat->id == $pro->categoria_id ?  'selected' : '';?>>
                 <?=$cat->nombre?>
             </option>
         <?php endwhile; ?>
     </select>
 
     <label for="imagen">Imagen</label>
+    <?php if(isset($pro) && is_object($pro) && !empty($pro->imagen)): ?>
+        <img src="<?base_url?>/uploads/<?=$pro->imagen?>" />
+    <?php endif; ?>   
     <input type="file" name="imagen" />
 
     <input type="submit" value="Guardar">
